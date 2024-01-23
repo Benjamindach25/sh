39ac618d3281d6519ed43482ed303f5c80e77188
Mayavar258 <123068420+Mayavar258@users.noreply.github.com>
2024-01-23 09:23:12 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -105,13 +105,12 @@ ABOUT_TEXT = """
 
 `🧰 Framework:` [Pyrogram](https://github.com/pyrogram/pyrogram)
 
-`👨‍💻 Developer:` [Anonymous](t.me/DKBOTZHELP_2)
+`👨‍💻 Developer:` [Anonymous](https://t.me/Anlinks_in_support)
 
-`📢 Support:` [Anonymous](https://t.me/DKBOTZ)
+`📢 Support:` [Anonymous](https://t.me/ANLINKS_IN)
 
-`🌐 Source Code:` **[Click Here](https://t.me/DKBOTZHELP_2)**
+`🌐 Source Code:` **[Click Here](https://t.me/Anlinks_in_support)**
 """
-
 
 CUSTOM_ALIAS_MESSAGE = """For Custom Alias, `[link] | [custom_alias]`, Send in this format
 
