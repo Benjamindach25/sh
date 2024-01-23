eb0f9962a8414976b06b7bdb937238465f6277fd
Muhammedshifin <muhammedshifink4@gmail.com>
2024-01-23 21:38:29 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -87,7 +87,7 @@ async def balance_command_by_dkbotz(c, m: Message):
     except Exception as e:
         await m.reply_text(e)
 
-@Client.on_message(filters.command('about'))
+@Client.on_message(filters.command('about') & filters.private)
 async def about_command(c, m: Message):
     reply_markup=ABOUT_REPLY_MARKUP
     bot = await c.get_me()
