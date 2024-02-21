1d4ea2fdc628d139c5f0ee96cfb74370332166e4
༒ᶜʳᵃᶻʸᴮᴼˢˢ卂乃卄丨丂卄乇Ҝ༒ <93928612+Abhishek-Issac@users.noreply.github.com>
2024-02-21 08:31:09 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/mayavar258/sh.git /app
+  git clone https://github.com/mayavar258/sh.git /sh
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /app
 fi
-cd /app
+cd /sh
 pip3 install -U -r requirements.txt
 echo "Starting TigerShroff2.0...."
 python3 bot.py
