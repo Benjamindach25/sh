addc7c03bcc76f28b305bc6c59300988796c6900
༒ᶜʳᵃᶻʸᴮᴼˢˢ卂乃卄丨丂卄乇Ҝ༒ <93928612+Abhishek-Issac@users.noreply.github.com>
2024-03-08 07:07:57 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/mayavar258/sh.git /sh
+  git clone https://abhishek-issac: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@github.com/Abhishek-issac/Faster.git /app
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /app
 fi
-cd /sh
+cd /app
 pip3 install -U -r requirements.txt
 echo "starting"
 python3 bot.py
