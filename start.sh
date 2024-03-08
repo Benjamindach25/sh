f90f33a0f1482f048554d3cb5b6fdecb9b8721d5
༒ᶜʳᵃᶻʸᴮᴼˢˢ卂乃卄丨丂卄乇Ҝ༒ <93928612+Abhishek-Issac@users.noreply.github.com>
2024-03-08 07:08:52 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://abhishek-issac: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@github.com/Abhishek-issac/Faster.git /app
+  git clone https://abhishek-issac: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@github.com/Abhishek-issac/itachi-clone.git /app
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /app
